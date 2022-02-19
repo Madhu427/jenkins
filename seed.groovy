e83ce0a21e22566a7c3e8ee0ef3865d6c77829cb
Madhu427 <madhusudhanachary.k@gmail.com>
2022-02-19 22:40:44 +0530

    seed.groovy file --> template added

--- seed.groovy
+++ seed.groovy
@@ -1,4 +1,4 @@
-job('example') {
+job('example1') {
     logRotator(-1, 10)
     jdk('Java 8')
     scm {
@@ -14,3 +14,25 @@ job('example') {
         archiveArtifacts('job-dsl-plugin/build/libs/job-dsl.hpi')
     }
 }
+
+
+//pipelineJob('frontend') {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/frontend')
+//          }
+//        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
+//    }
+//  }
+//}
