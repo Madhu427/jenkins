37204b582281d5ea1057883db1845c4edf482d83
Madhu427 <madhusudhanachary.k@gmail.com>
2022-02-26 17:32:46 +0530

    Adding Folder 'CI-Pipelines'

--- seed.groovy
+++ seed.groovy
@@ -36,3 +36,30 @@ job('example1') {
 //    }
 //  }
 //}
+
+
+folder('CI-Pipelines') {
+    displayName('CI-Pipelines')
+    description('CI-Pipelines')
+}
+
+pipelineJob('CI-Pipelines/cart') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/Madhu427/cart1.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
