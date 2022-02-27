de7fa2108bc8dcc6880d31a8a404df772ba28836
Madhu427 <madhusudhanachary.k@gmail.com>
2022-02-27 08:27:24 +0530

    nodejs.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,34 +1,40 @@
-pipeline {
+def call() {
+    pipeline {
 
 
-    agent {
-        label 'WORKSTATION'
-    }
+        agent {
+            label 'WORKSTATION'
+        }
 
-    triggers {
+        triggers {
 
-        pollSCM('*/1 * * * *')
-
-
-    }
+            pollSCM('*/1 * * * *')
 
 
-    stages{
-        stage('Compile the code') {
-            steps{
-                sh 'echo compile the code'
+        }
+
+
+        stages{
+            stage('Compile the code') {
+                steps{
+                    sh 'echo compile the code'
+                }
+            }
+            stage('Test the code') {
+                steps{
+                    sh 'echo test the code'
+                }
+            }
+            stage('Deploy the code') {
+                steps{
+                    sh 'echo deploy the code'
+                }
             }
         }
-        stage('Test the code') {
-            steps{
-                sh 'echo test the code'
-            }
-        }
-        stage('Deploy the code') {
-            steps{
-                sh 'echo deploy the code'
-            }
-        }
+
     }
+
 
 }
+
+
