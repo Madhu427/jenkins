2a2f3b50ac6a57af772cd684b341122d25c4751c
Madhu427 <madhusudhanachary.k@gmail.com>
2022-02-27 10:28:11 +0530

    java,golang and python.groovy files added

---
+++ vars/python.groovy
@@ -0,0 +1,40 @@
+def call() {
+    pipeline {
+
+
+        agent {
+            label "${BUILD_LABEL}"
+        }
+
+        triggers {
+
+            pollSCM('*/1 * * * *')
+
+
+        }
+
+
+        stages{
+            stage('Compile the code') {
+                steps{
+                    sh 'echo compile the ${COMPONENT}  code'
+                }
+            }
+            stage('Test the code') {
+                steps{
+                    sh 'echo test the code'
+                }
+            }
+            stage('Deploy the code') {
+                steps{
+                    sh 'echo deploy the code'
+                }
+            }
+        }
+
+    }
+
+
+}
+
+
