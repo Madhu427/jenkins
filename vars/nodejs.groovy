6517482b40a1f1e0a1bf0132f0ac52a02b857a39
Madhu427 <madhusudhanachary.k@gmail.com>
2022-02-27 08:24:29 +0530

    nodejs.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,7 +1,34 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
+pipeline {
 
-def warning(message) {
-    echo "WARNING: ${message}"
+
+    agent {
+        label 'WORKSTATION'
+    }
+
+    triggers {
+
+        pollSCM('*/1 * * * *')
+
+
+    }
+
+
+    stages{
+        stage('Compile the code') {
+            steps{
+                sh 'echo compile the code'
+            }
+        }
+        stage('Test the code') {
+            steps{
+                sh 'echo test the code'
+            }
+        }
+        stage('Deploy the code') {
+            steps{
+                sh 'echo deploy the code'
+            }
+        }
+    }
+
 }
