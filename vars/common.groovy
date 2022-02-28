3c87031eec37afe2cc0598c811929c39c27b864b
Madhu427 <madhusudhanachary.k@gmail.com>
2022-02-28 18:56:13 +0530

    sonar-scanner -->credentials given

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,3 @@
 def sonarQube() {
-    println "sonarqube testing"
+    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.6.52:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 }
