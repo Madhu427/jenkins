c08436b3604c638dc39dd7b6246c239362258b38
Madhu427 <madhusudhanachary.k@gmail.com>
2022-02-28 18:36:32 +0530

    def sonarQube() --> removed

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -25,7 +25,7 @@ def call() {
                 steps{
                     sh 'echo test the code'
                      script {
-                        common.sonarQube("cart","nodejs")
+                        common.sonarQube()
                      }
                 }
             }
