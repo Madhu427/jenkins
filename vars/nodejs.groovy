7a3b0d019ef9ae65404bdf07df38561f9fdd93d9
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-01 19:23:59 +0530

    stage('Publish Artifacts') --> added

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -49,8 +49,8 @@ def call() {
                 }
             }
 
-            post {
-                always {
+            post{
+                always{
                     cleanWs()
                 }
             }
