726f0a814f39fc0971b28265f867e79d0190fe0a
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-01 19:35:49 +0530

    stage('Publish Artifacts') --> added

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,5 @@
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+
 def sonarQube() {
 //    sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.6.52:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 //    sh ' sonar-quality-gate.sh admin admin123 172.31.6.52 ${COMPONENT}'
