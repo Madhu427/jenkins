4c5c70808a6d0f0279c054e7968f36cc6fa368c9
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-03 12:07:28 +0530

    NEXUS = credentials('NEXUS') missed in env

--- vars/common.groovy
+++ vars/common.groovy
@@ -15,7 +15,7 @@ def publishArtifacts() {
 //  }
 
     sh'''
-      curl -f -v -u admin:admin123 --upload-file ${COMPONENT}-${gitTag}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${gitTag}.zip
+      curl -f -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${gitTag}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${gitTag}.zip
   '''
 }
 
