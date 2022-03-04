0fae4d5819d0c722fdde2d3985fb81d603cb1764
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-04 19:09:10 +0530

    java artifacts prepared

--- vars/common.groovy
+++ vars/common.groovy
@@ -31,8 +31,8 @@ def prepareArtifacts() {
     if(env.PROG_LANG_NAME == "java" && env.PROG_LANG_VERSION == "1.8") {
         sh '''
         mvn clean package
-        mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar'''
-        zip -r ${COMPONENT}-${gitTag}.zip ${COMPONENT}.jar
+        mv target/${COMPONENT}-1.0.jar ${COMPONENT}.jar
+        zip -r ${COMPONENT}-${gitTag}.zip ${COMPONENT}.jar'''
     }
 
 }
