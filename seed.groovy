e30cfc04cbf4d338d917abc4ef77582609e25201
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-12 15:10:18 +0530

    Jenkinsfile terraform,groovy added, seed.groovy added

--- seed.groovy
+++ seed.groovy
@@ -72,3 +72,29 @@ for(i in 0..SIZE){
     }
 }
 
+folder('Mutable') {
+    displayName('Mutable')
+    description('Mutable')
+}
+
+pipelineJob("Mutable/VPC") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'("https://github.com/Madhu427/terraform-vpc.git")
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/master')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+}
