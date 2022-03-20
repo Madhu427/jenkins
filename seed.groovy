fefb910684e38c78cb4a0d09e432c5037b6f49a7
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-20 22:22:25 +0530

    Mutable --> DB

--- seed.groovy
+++ seed.groovy
@@ -141,3 +141,23 @@ pipelineJob('Mutable/ALB') {
     }
 }
 
+pipelineJob('Mutable/DB') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+            'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'("https://github.com/Madhu427/terraform-db.git")
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/master')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
