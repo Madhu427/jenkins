86a45c2be956992301cd10dbb6b697619ce9c729
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-22 13:36:30 +0530

    jenkins file & seed.groovy for All infra create

--- seed.groovy
+++ seed.groovy
@@ -161,3 +161,24 @@ pipelineJob('Mutable/DB') {
         }
     }
 }
+
+pipelineJob('Mutable/ALL-Infra-Create') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+            'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'("https://github.com/Madhu427/jenkins.git")
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/master')
+                    }
+                }
+            }
+            'scriptPath'('jenkinsfile-Mutable-Infra-All-in-one-create')
+            'lightweight'(true)
+        }
+    }
+}
